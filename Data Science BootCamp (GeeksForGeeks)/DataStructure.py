fdbf8af8e0d3275d8ee0e5253520b567e0f43651
FjRabbi <fajle15-12317@diu.edu.bd>
2023-03-30 11:00:56 +0600

    List

---
+++ Data Science BootCamp (GeeksForGeeks)/DataStructure.py
@@ -0,0 +1,14 @@
+print()
+# ----------- Topic: Data Structure -----------
+print("----------- Topic: Data Structure -----------")
+print()
+
+# ----------- List Introduction -----------
+print("----------- list Introduction -----------")
+print()
+
+# Python Lists are just like dynamically sized arrays, declared in other languages (vector in C++ and ArrayList in Java).
+# In simple language, a list is a collection of things, enclosed in [ ] and separated by commas.
+
+
+
