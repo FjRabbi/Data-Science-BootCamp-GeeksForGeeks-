690f679a8d80704b2921a2479e778caae4207318
FjRabbi <fajle15-12317@diu.edu.bd>
2023-03-31 19:30:52 +0600

    note

--- Data Science BootCamp (GeeksForGeeks)/DataStructure.py
+++ Data Science BootCamp (GeeksForGeeks)/DataStructure.py
@@ -9,6 +9,9 @@ print()
 
 # Python Lists are just like dynamically sized arrays, declared in other languages (vector in C++ and ArrayList in Java).
 # In simple language, a list is a collection of things, enclosed in [ ] and separated by commas.
+#The list is a sequence data type which is used to store the collection of data. Tuples and String are other types of sequence data types.
 
+var = ['geeks', 'for', 'geeks']
+print(var)
 
 
